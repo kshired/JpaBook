2cd3ee7dd81067dd5d9c5363ec5ae915cc134470
kshired <shiroed1211@naver.com>
2021-06-22 00:46:05 +0900

    order update

---
+++ src/main/java/jpabook/jpabook/service/OrderService.java
@@ -0,0 +1,44 @@
+package jpabook.jpabook.service;
+
+import jpabook.jpabook.domain.*;
+import jpabook.jpabook.domain.item.Item;
+import jpabook.jpabook.repository.ItemRepository;
+import jpabook.jpabook.repository.MemberRepository;
+import jpabook.jpabook.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+
+}
